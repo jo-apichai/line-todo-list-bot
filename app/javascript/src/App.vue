fa543dfb671e3cb9e50574bcc31617f161d11418
Jo Apichai <jo.midnight@gmail.com>
2018-06-02 01:59:22 +0700

    Show basic tasks list

--- app/javascript/src/App.vue
+++ app/javascript/src/App.vue
@@ -1,14 +1,36 @@
 <template>
   <div id="app">
-    {{ message }}
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.datetime }} - {{ task.description }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import 'whatwg-fetch'
+
   export default {
     data () {
       return {
-        message: 'Hello Vue!'
+        tasks: []
+      }
+    },
+
+    mounted: function () {
+      this.loadTasks()
+    },
+
+    methods: {
+      loadTasks: function () {
+        fetch('/api/v1/tasks', { credentials: 'same-origin' })
+          .then((response) => {
+            return response.json()
+          })
+          .then((json) => {
+            this.tasks = json
+          })
       }
     }
   }
--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,3 +1,7 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :description, :datetime, :finished, :important
+
+  def datetime
+    object.datetime.strftime('%-d/%-m/%y %H:%M')
+  end
 end
